6c015da3bb59515237a14fca79fe7ccc39f3f837
sarthakdixit7376 <dixitsarthak004@gmail.com>
2026-07-16 08:59:21 +0530

    added railway backend link to lead

--- client/src/Components/CustomerTabs/Lead.tsx
+++ client/src/Components/CustomerTabs/Lead.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const API_BASE = 'http://localhost:4000';
+const API_BASE = 'https://customcrm-production.up.railway.app';
 
 export interface LeadRow {
   id: string;
